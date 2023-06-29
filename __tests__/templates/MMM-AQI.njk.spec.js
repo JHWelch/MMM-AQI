fedc9a8b6bc6110e30ad20e9ea5a7829d37fa71d
Jordan Welch <jordanhwelch@gmail.com>
2023-06-28 19:51:44 -0500

    Move nunjucks to mock

--- __tests__/templates/MMM-AQI.njk.spec.js
+++ __tests__/templates/MMM-AQI.njk.spec.js
@@ -1,12 +1,10 @@
-const nunjucks = require('nunjucks');
+nunjucks = require('../../__mocks__/nunjucks');
 
 translate = (str) => str;
 
 describe('MMM-AQI.njk', () => {
   describe('content loaded', () => {
     it('shows the AQI', () => {
-      nunjucks.configure(['templates']);
-
       const payload = {
         loading: false,
         aqi: 179,
@@ -19,14 +17,7 @@ describe('MMM-AQI.njk', () => {
 
   describe('loading', () => {
     it('shows loading', () => {
-      const nunjucksEnvironment = nunjucks.configure(['templates']);
-      nunjucksEnvironment.addFilter(
-        'translate',
-        (str, variables) => nunjucks.runtime.markSafe(translate(str, variables)),
-      );
-      const payload = {
-        loading: true,
-      };
+      const payload = { loading: true };
       const template = nunjucks.render('MMM-AQI.njk', payload);
 
       expect(template).toContain('LOADING');
